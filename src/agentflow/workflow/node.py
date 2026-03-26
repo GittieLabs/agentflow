469ac032fc54664cf5bcfc13ec8f059a8d411d46
Keith Elliott <mrkeithelliott@users.noreply.github.com>
2026-03-26 10:48:58 -0400

    feat(workflow): inject all named inputs into workflow variables for Jinja rendering

--- src/agentflow/workflow/node.py
+++ src/agentflow/workflow/node.py
@@ -97,6 +97,12 @@ class NodeRunner:
         # Resolve input message from prior node outputs
         message = self._resolve_message(prior_outputs)
 
+        # Map all inputs to variables for Jinja templating
+        merged_variables = dict(variables) if variables else {}
+        for key, ref in self._node.inputs.items():
+            # 'message' is passed explicitly, but we can also make it available as a variable
+            merged_variables[key] = self._resolve_ref(ref, prior_outputs)
+
         # Write incoming context to scratchpad
         if self._scratchpad and prior_outputs:
             context_parts = []
@@ -111,7 +117,7 @@ class NodeRunner:
             message=message,
             session_id=session_id,
             node_id=self._node.id,
-            variables=variables,
+            variables=merged_variables,
         )
 
         # Write output summary to scratchpad
